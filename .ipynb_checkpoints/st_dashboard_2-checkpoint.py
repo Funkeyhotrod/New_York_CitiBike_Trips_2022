7c35b15deda682a44ae43936134d7b05c1b77712
Aaron Johnson <aaronpjohnson2001@gmail.com>
2025-05-13 14:50:29 -0700

    Last wave

--- .ipynb_checkpoints/st_dashboard_2-checkpoint.py
+++ .ipynb_checkpoints/st_dashboard_2-checkpoint.py
@@ -32,11 +32,13 @@ top20 = pd.read_csv('top20.csv', index_col = 0)
 
 if page == 'Intro page':
     st.markdown('## Where are all the bikes?')
-    st.markdown('CitiBike is having a lot of issues with the disribution of its bikes across New York City. This analysis aims to find any possible explanation for this, and how to fix it.')
-    st.markdown('- Weather component and bike usage')
-    st.markdown('- Most popular stations')
-    st.markdown('- Interactive map with aggregated bike trips')
-    st.markdown('- Recommendations')
+    st.markdown('CitiBike is a bike sharing/rental system located in New York City. Throughout the city are many bike stations that people can walk up to and rent out a bike for the day, and afterwards they can return the bike to any of the stations.')
+    st.markdown('CitiBike is having a lot of issues with the disribution of bikes across its many stations, with some of them ether being empty or overstocked with bikes.')
+    st.markdown('The objective of this analysis is to find any possible explanations for why this is happening, and making recommendations to fix it.')
+    st.markdown("To do this I'll be going through the data CitiBike has on each trip that was taken during 2022 with these questions as my research starting points.")
+    st.markdown('- Does weather have an effect on the number of trips?')
+    st.markdown('- What are the most popular starting stations?')
+    st.markdown('- What are the most common bike trips?')
     st.markdown("The dropdown menu on the left 'Aspect Selector' will take you to the different aspects of the analysis our team looked at.")
     myImage = Image.open('Citi Bikes.jpg') #source: https://www.google.com/url?sa=i&url=https%3A%2F%2Fny1.com%2Fnyc%2Fall-boroughs%2Ftransit%2F2024%2F01%2F03%2Fciti-bike-price-increases-to-start-taking-effect-thursday&psig=AOvVaw2GYf1GYjDl4o6t-vBBJiAc&ust=1745878036105000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCICH0Yed-YwDFQAAAAAdAAAAABAE
     st.image(myImage)
@@ -65,7 +67,8 @@ elif page == 'Weather component and bike usage':
     )
 
     st.plotly_chart(fig_2, use_container_width=True)
-    st.markdown('We can see from this graph that the daily temprature and the number of riders are directly correlated with each other. People are more likely to go outside, and ride one of our bikes, during warmer wether in order to enjoy the nice day. From this we can conclude that the disribution issues peak during the summer.')
+    st.markdown('We can see from this graph that the daily temprature and the number of bike trips are directly correlated with each other. People are more likely to go outside, and ride one of our bikes, during warmer wether in order to enjoy the nice day.')
+    st.markdown('From this we can conclude that bikes will be used a lot more during the summer, when the demand for bikes is at its highest. And during the winter, when demand is at its lowest, there would be a lot less bikes being used')
 
 ################################## Bar Chart ############################################################################
 
@@ -79,7 +82,9 @@ elif page == 'Most popular stations':
     width = 900, height = 600
     )
     st.plotly_chart(fig, use_container_width=True)
-    st.markdown("From this bar chart it's easy to see that the top 5 starting stations are way more popular than the other stations, with all the top 5 stations being used over 100,000 time in one year. The gaps between the top 4 stations are also massive, with increases around 8,000 users, and the most popular starting station, W 21 St & 6 Ave, almost totaling 130,000 users. These are major hot spots that we need to pay attention to.")
+    st.markdown('This bar chart shows the 20 most frequented starting stations, with all of them having been used over 85,000 times in one year. That averages out to having over 230 users each day for each of those 20 station.')
+    st.markdown('The most frequented starting station is W 21 St/6 Ave, with its total number of trips during 2022 being 129,000. This averages out to having over 350 users each day.')
+    st.markdown('With the immense amount of people using these more popular stations, the chances of these stations losing there supply of bikes throughout the day and being compleatly empty when more people show up are very high.')
 
 ################################## Adding Map ###########################################################################
 
@@ -95,9 +100,10 @@ elif page == 'Interactive map with aggregated bike trips':
     ## Show in webpage
     st.header("Aggregated Bike Trips in New York City (Interactive map)")
     st.components.v1.html(html_data,height=1000)
-    st.markdown("#### Using the filter on the left hand side of the map we can check the 5 most popular trips, and compare them with the most popular starting stations.")
-    st.markdown('The top 2 starting stations from the bar chart, W 21 St/6 Ave and West St/Chambers St, show up again in this map. And W 21 St/6 Ave once again takes the top spot. Yet the other three popular trips have starting stations outside of the top 20 bar chart.')
-    st.markdown("It's also important to note that in this filter, we see that the vast majority of trips start and end at the same location, and are spread throughout the city.")
+    st.markdown("This map has been filtered to only show the bike trips that have been used over 3,000 times. At a glance there's no obvious pattern to where these common trips are located.")
+    st.markdown('Even though there are no specific landmarks, or geographic trait that draw people in, we can see from this map that there are many dense clusters of bike trips that are only distributed between a few stations.')
+    st.markdown('Also, the most common trip between two different stations starts at the most frequented starting stations we looked at earlier, W 21 St/6 Ave.')
+    st.markdown('These dense clusters of trips have a high volume of users that can quickly drain the supply of bikes from the nearby stations.')
 
 ################################## Recommendations ######################################################################
 
@@ -107,6 +113,6 @@ else:
     bikes = Image.open('Citi Bikes2.webp')  #source: https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.groupon.com%2Fdeals%2Fciti-bike&psig=AOvVaw2GYf1GYjDl4o6t-vBBJiAc&ust=1745878036105000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCICH0Yed-YwDFQAAAAAdAAAAABAK
     st.image(bikes)
     st.markdown('### From this analysis I have 3 recommendations for what CitiBikes can do moving forward:')
-    st.markdown('- Add more stations around the varius hot spot locations around the city, like W 21 St/6 Ave and West St/Chambers St')
-    st.markdown('- Make sure that these more popular stations are fully stocked during the summer to keep up with demand, and reduce the number of bikes during the winter to prevent the stations from being congested with unused bikes')
-    st.markdown('- Conducting further analysis regarding frequent end stations, and finding the difference between a station being used as a starting point and an ending point of a trip')
+    st.markdown('- Adding 1 or 2 new bike stations in locations with a dense amount of trips, and by the most used starting stations, will provide more bikes to areas with high amounts of user activity.')
+    st.markdown('- Make sure that these more popular stations are fully stocked during the summer to keep up with demand, and reduce the number of available bikes by 40% or 50% during the winter to prevent the stations from being congested with unused bikes.')
+    st.markdown('- Conducting a further analysis on both starting and ending stations to see which stations are gaining more bikes, or losing more bikes throughout the day. This info can help determin which stations are in need for more bikes in there givin area.')
